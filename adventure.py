3fbb5557e9881f13649fb2559f982a0c202ca4d5
Anna Welpa <anna.welpa@gmail.com>
2022-08-17 22:00:25 +0200

    Add subclasses of protagonist and npc

--- adventure.py
+++ adventure.py
@@ -1,16 +1,14 @@
 # Text based little game to practice Python
 
-class Protagonist:
-    """A class containing basic information about our hero"""
-    def __init__(self, name, race, alignment=0, muscle=0, magic=0):
+class Character:
+    """A class containing basic information about characters"""
+    def __init__(self, name, race, inventory):
         self.name = name
         self.race = race
-        self.alignment = alignment
-        self.muscle = muscle
-        self.magic = magic
+        self.inventory = inventory
 
     def __repr__(self):
-        return Protagonist(self.name, self.race)
+        return Character(self.name, self.race)
 
     def __str__(self):
         return(str(self.name + ' The ' + self.race))
@@ -18,18 +16,52 @@ class Protagonist:
     def showYourself(self):
         print('I AM {} THE GREAT {}. Witness my power!'.format(self.name, self.race).upper())
 
-    def changeAlign(self, alignNum):
-        self.alignment = self.alignment + alignNum
+    def showInventory(self):
+        print('Currently in your inventory:')
+        for item in self.inventory:
+            print('* ', item)
 
-    def changeMuscle(self, muscleNum):
-        self.muscle = self.muscle + muscleNum
+class Protagonist(Character):
+    def __init__(self, name, race, inventory, alignment=0, magic=0):
+        super().__init__(name, race, inventory)
+        self.alignment = alignment
+        self.magic = magic
+
+    def changeAlign(self,  alignNum):
+        self.alignment = self.alignment + alignNum
 
     def changeMagic(self, magiceNum):
         self.magic = self.magic + magiceNum
 
+class Npc(Character):
+    def __init__(self, name, race, inventory, friend=0):
+        super().__init__(name, race, inventory)
+        self.friend = friend
 
-#hero = Protagonist('Bob', 'Dwarf')
-#hero.showYourself()
+    def changeFriend(self, friendNum):
+        self.friend = self.friend + friendNum
+
+hop = Npc('Hop', 'Goblin', ['old coin'])
+hop.showYourself()
+
+hero = Protagonist('Bob', 'Dwarf', ['amulet', 'torch'])
+hop.showYourself()
+hop.showInventory()
+
+import time
+
+print('''--You wake up in a dark cold place. You feel the rocks beneath you. You also feel strangely calm,
+(as if you didn't just wake up in a dark cold place). You begin to lift your head from the ground to take a look around when suddenly...--''')
+#input("Press Enter to continue...")
+print('\nHello there my dear adventurer. Fancy seeing you there!')
+print('--You hear a friendly voice, booming through your surroundings--')
+print('''We are in quite a predicament, aren't we?''')
+
+ans = input()
+if ans == 'Who are you?':
+    hero.changeAlign(-1)
+
+
 
 
 
